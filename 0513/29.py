53abb25ad0d5f72258a7e6375a60bb7e7d340ea1
leemin123456 <sm895959@gmail.com>
2024-05-13 14:39:01 +0900

    05-13

--- 0513/29.py
+++ 0513/29.py
@@ -12,3 +12,26 @@ class BasicBlock(nn.Module):
                             kernel_size=kernel_size, padding=1)
         self.downsample = nn.Conv2d(in_channels, out_channels,
                             kernel_size=1)
+
+        self.fn1 = nn.BatchNorm2d(num_features=out_channels)
+        self.fn2 = nn.BatchNorm2d(num_features=out_channels)
+
+        self.relu = nn.ReLU
+
+    def forward(self, x):
+
+        x_ = x
+
+        x = self.c1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.c2(x)
+        x = self.bn2(x)
+
+        x_ = self.downsample(x_)
+
+        x +=x_
+        x = self.relu(x)
+
+        return x
+
