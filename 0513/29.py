7c3ac0d1834a3fec9e323acc9eb70171d6ac2cc3
minlee123456 <sm895959@gmail.com>
2024-05-14 14:08:16 +0900

    05-14

--- 0513/29.py
+++ 0513/29.py
@@ -11,7 +11,7 @@ class BasicBlock(nn.Module):
         self.c2 = nn.Conv2d(out_channels, in_channels,
                             kernel_size=kernel_size, padding=1)
         self.downsample = nn.Conv2d(in_channels, out_channels,
-                            kernel_size=1)
+                                    kernel_size=1)
 
         self.fn1 = nn.BatchNorm2d(num_features=out_channels)
         self.fn2 = nn.BatchNorm2d(num_features=out_channels)
@@ -19,7 +19,6 @@ class BasicBlock(nn.Module):
         self.relu = nn.ReLU
 
     def forward(self, x):
-
         x_ = x
 
         x = self.c1(x)
@@ -30,8 +29,101 @@ class BasicBlock(nn.Module):
 
         x_ = self.downsample(x_)
 
-        x +=x_
+        x += x_
         x = self.relu(x)
 
         return x
 
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ResNet, self).__init__()
+
+        self.b1 = BasicBlock(in_channels=3, out_channels=64)
+        self.b2 = BasicBlock(in_channels=64, out_channels=128)
+        self.b3 = BasicBlock(in_channels=128, out_channels=256)
+
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(in_features=4096, out_features=2048)
+        self.fc2 = nn.Linear(in_features=2048, out_features=512)
+        self.fc3 = nn.Linear(in_features=512, out_features=num_classes)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.b1(x)
+        x = self.pool(x)
+        x = self.b2(x)
+        x = self.pool(x)
+        x = self.b3(x)
+        x = self.pool(x)
+
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+
+        return x
+
+
+import tqdm
+
+from torchvision.datasets.cifar import CIFAR10
+from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import RandomHorizontalFlip, RandomCrop
+from torchvision.transforms import Normalize
+from torch.utils.data.dataloader import DataLoader
+
+from torch.optim.adam import Adam
+
+transforms = Compose([
+    RandomCrop((32, 32), padding=4),
+    RandomHorizontalFlip(p=0.5),
+    ToTensor(),
+    Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.247, 0.243, 0.261))
+])
+
+training_data = CIFAR10(root="./", train=True, download=True, transform=transforms)
+test_data = CIFAR10(root="./", train=False, download=True, transform=transforms)
+
+train_loader = DataLoader(training_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = ResNet(num_classes=10)
+model.to(device)
+
+lr = 1e-4
+optim = Adam(model.parameters(), lr=lr)
+
+for epoch in range(30):
+    iterator = tqdm.tqdm(train_loader)
+    for data, label in iterator:
+        optim.zero_grad()
+
+        preds = model(data.to(device))
+
+        loss = nn.CrossEntropyLoss()(preds, label.to(device))
+        loss.backward()
+        optim.step()
+        iterator.set_description(f"epoch:{epoch + 1}, loss:{loss.item()}")
+
+torch.save(model.state_dict(), "ResNet.pth")
+
+model.load_state_dict(torch.load("ResNet.pth"))
+
+num_corr = 0
+
+with torch.no_grad():
+    for data, label in test_loader:
+        output = model(data.to(device))
+        preds = output.data.max(1)[1]
+        corr = preds.eq(label.to(device).data).sum().item()
+        num_corr += corr
+
+    print(f"Accuracy:{num_corr / len(test_data)}")
