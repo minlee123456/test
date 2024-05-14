db14be6b8a2de0116b283f90e75237e45ba9e33c
minlee123456 <sm895959@gmail.com>
2024-05-14 14:43:32 +0900

    05-14

--- 0513/29.py
+++ 0513/29.py
@@ -3,20 +3,20 @@ import torch.nn as nn
 
 
 class BasicBlock(nn.Module):
-    def __inti__(selfself, in_channels, out_channels, kernel_size=3):
-        super(baicBlock, self).__init__()
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        super(BasicBlock, self).__init__()
 
         self.c1 = nn.Conv2d(in_channels, out_channels,
                             kernel_size=kernel_size, padding=1)
-        self.c2 = nn.Conv2d(out_channels, in_channels,
+        self.c2 = nn.Conv2d(out_channels, out_channels,
                             kernel_size=kernel_size, padding=1)
         self.downsample = nn.Conv2d(in_channels, out_channels,
                                     kernel_size=1)
 
-        self.fn1 = nn.BatchNorm2d(num_features=out_channels)
-        self.fn2 = nn.BatchNorm2d(num_features=out_channels)
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
 
-        self.relu = nn.ReLU
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x_ = x
